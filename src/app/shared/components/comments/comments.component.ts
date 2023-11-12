bfca66eb45ceff0087aea34f445b5e4bae74875b
Divini <92796539+Diviini@users.noreply.github.com>
2023-11-12 15:44:10 +0100

    Animation complexe

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,75 +1,112 @@
-import { state, trigger, style, transition, animate } from '@angular/animations';
+import {state,trigger,style,transition,animate,query,group,sequence,stagger, animateChild, useAnimation,} from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
+import { flashAnimation } from '../../animations/flash.animation';
+import { sldieAndFadeAnimation } from '../../animations/slide-and-fade.animation';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
   animations: [
-    trigger('listItem', [
-      state('default', style({
-        transform: 'scale(1)',
-        'background-color': 'white',
-        'z-index': 1
-      })),
-      state('active', style({
-        transform: 'scale(1.05)',
-        'background-color': 'rgb(201,157,242)',
-        'z-index': 2
-      })),
-      transition('default => active', [
-        animate('100ms ease-in-out')
-      ]),
-      transition('active => default', [
-        animate('500ms ease-in-out')
-      ]),
+    trigger('list', [
       transition(':enter', [
-        style({
-          transform: 'translateX(-100%)',
-          opacity:0,
-          'background-color': 'rgb(201,157,242)',
-        }),
-        animate('250ms ease-out', style({
-          transform: 'translateX(0)',
-          opacity: 1,
-          'background-color': 'white',
-        }))
+        query('@listItem', [
+          stagger(50, [
+            animateChild()
+          ])
+        ])
       ])
-    ])
-  ]
+    ]),
+    trigger('listItem', [
+      state(
+        'default',
+        style({
+          transform: 'scale(1)',
+          'background-color': 'white',
+          'z-index': 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201,157,242)',
+          'z-index': 2,
+        })
+      ),
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')]),
+      transition(':enter', [
+        query('.comment-texte, .comment-date', [
+          style({
+            opacity: 0,
+          }),
+        ]),
+        useAnimation(sldieAndFadeAnimation, {
+          params: {
+            time: '1000ms',
+            startColor: 'rgb(256,0,0)'
+          }
+        }),
+        group([
+          useAnimation(flashAnimation, {
+            params: {
+              time: '1000ms',
+              flashColor: 'rgb(249,179,111)'
+            }
+          }),
+          style({
+            'background-color': 'rgb(255,7,147)'
+          }),
+          query('.comment-text', [
+            animate('250ms',style({
+              opacity: 1,
+            }))
+          ]),
+          query('.comment-date', [
+            animate('500ms',style({
+              opacity: 1,
+            })),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class CommentsComponent implements OnInit {
-
   @Input() comments!: Comment[];
   @Output() newComment = new EventEmitter<string>();
-  
+
   tempDate = new Date();
 
   commentCtrl!: FormControl;
-  animationStates: {[key: number]: 'default' | 'active'} = {}
+  animationStates: { [key: number]: 'default' | 'active' } = {};
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    this.commentCtrl = this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]);
     for (let index in this.comments) {
-      this.animationStates[index]='default';
+      this.animationStates[index] = 'default';
     }
   }
 
   onLeaveComment() {
-    if(this.commentCtrl.invalid) {
+    if (this.commentCtrl.invalid) {
       return;
     }
-    const maxId = Math.max(...this.comments.map(comment => comment.id));
+    const maxId = Math.max(...this.comments.map((comment) => comment.id));
     this.comments.unshift({
       id: maxId + 1,
       comment: this.commentCtrl.value,
       createdDate: new Date().toString(),
-      userId: 1
-    })
+      userId: 1,
+    });
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
   }
@@ -81,5 +118,4 @@ export class CommentsComponent implements OnInit {
   onListItemMouseLeave(index: number) {
     this.animationStates[index] = 'default';
   }
-  
 }
