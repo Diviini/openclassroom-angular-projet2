2b0a837ebeddb4ef8924218792d18727f3212f60
Divini <92796539+Diviini@users.noreply.github.com>
2023-11-12 14:32:02 +0100

    Animation sur les commentaies

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,3 +1,4 @@
+import { state, trigger, style, transition, animate } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
@@ -6,6 +7,26 @@ import { Comment } from 'src/app/core/models/comment.model';
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201,157,242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -15,11 +36,15 @@ export class CommentsComponent implements OnInit {
   tempDate = new Date();
 
   commentCtrl!: FormControl;
+  animationStates: {[key: number]: 'default' | 'active'} = {}
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    for (let index in this.comments) {
+      this.animationStates[index]='default';
+    }
   }
 
   onLeaveComment() {
@@ -30,5 +55,12 @@ export class CommentsComponent implements OnInit {
     this.commentCtrl.reset();
   }
 
+  onListItemMouseEnter(index: number) {
+    this.animationStates[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.animationStates[index] = 'default';
+  }
   
 }
