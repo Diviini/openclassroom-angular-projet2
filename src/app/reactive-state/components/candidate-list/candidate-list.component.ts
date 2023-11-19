0214dd179929e25905a51a26fc6be1bc2790a4c9
Diviini <92796539+Diviini@users.noreply.github.com>
2023-11-19 08:56:39 +0100

    Préparation du module pour la recherche

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CandidatesService } from '../../services/candidates.service';
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CandidateListComponent implements OnInit {
+
+  loading$!: Observable<boolean>;
+
+  constructor(private candidateService: CandidatesService) {}
+
+
+
+  ngOnInit(): void {
+    this.loading$ = this.candidateService.loading$;
+  }
+
+}
---
+++ src/app/reactive-state/services/candidates.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Candidate } from "../models/candidate.model";
+
+@Injectable()
+export class CandidatesService {
+    
+    constructor(private http: HttpClient) {}
+
+    private _loading$ = new BehaviorSubject<boolean>(false);
+    get loading$(): Observable<boolean>{
+        return this._loading$.asObservable();
+    }
+
+    private _candidate$ = new BehaviorSubject<Candidate[]> ([]);
+    get candidate$(): Observable<Candidate[]> {
+        return this._candidate$.asObservable();
+    }
+
+    private setLoadingStatus(loading: boolean) {
+        this._loading$.next(loading);
+    }
+}
